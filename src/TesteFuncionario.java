d11175c1ac683113a8e7f0e12613fafb7159fa46
BrandonCarlos <brandon_marzo@hotmail.com>
2022-07-18 09:43:15 -0300

    Classe abstrata não se instância, más podemos criar filhos concretos dessa classe

--- src/TesteFuncionario.java
+++ src/TesteFuncionario.java
@@ -1,7 +1,9 @@
 public class TesteFuncionario {
 	public static void main(String[] args) {
 		
-		Funcionario nico = new Funcionario("Nico Steppat", "223355646-9", 2600.00);
+		//Funcionario é uma classe "Abstract", não podemos mais criar instância de Funcionario, 
+		//só podemos criar filhos concretos da classe Funcionario
+		Funcionario nico = new Gerente("Nico Steppat", "223355646-9", 2600.00, 0);
 		
 		System.out.println(nico.getNome()); 
 		System.out.println(nico.getBonificacao()); 
